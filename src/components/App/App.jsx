12b827a511767d2661520bbe1e77b01d07c4c626
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2021-01-08 15:16:50 -0300

    add base layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Container, Wrapper } from './App.styles'
+import AppHeader from '../AppHeader'
+import AppContainer from '../AppContainer/AppContainer'
 
 function App(){
     return <Wrapper>
         <Container>
-            app
+            <AppHeader />
+            <AppContainer 
+                left={<div>produtos disponiveis</div>}
+                middle={<div>sua lista de compras</div>}
+                right={<div>estatisticas</div>}
+            />
         </Container>
     </Wrapper>
 }
