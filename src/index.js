caa96ac5b08b3b01cf34c81b247810f80f67f43e
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2021-01-08 10:07:53 -0300

    clear useless files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App () {
+  return <div>
+    Olá, mundo.
+  </div>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
